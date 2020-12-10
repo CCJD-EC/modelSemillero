e4b7f940c32c6f8e2585db492d9e956f97c8c739
Christian <jchristiandias@gmail.com>
2020-12-10 10:06:24 -0500

    Ejercicio 2 completado

--- src/main/java/com/clearminds/cca/dtos/Estudiante.java
+++ src/main/java/com/clearminds/cca/dtos/Estudiante.java
@@ -1,25 +1,31 @@
 package com.clearminds.cca.dtos;
 
+import java.sql.Date;
+
 public class Estudiante {
 
 	private String nombre;
 	private String apellido;
 	private int id;
 	private int edad; 
+	private String fechaModificacion;
 	
 	
-	public Estudiante(String nombre, String apellido, int id, int edad) {
+	public Estudiante(String nombre, String apellido, int id, int edad, String fecha_modificacion) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
 		this.edad= edad;
+		this.fechaModificacion = fecha_modificacion;
 	}	
 	
-	public Estudiante(String nombre, String apellido) {
+	public Estudiante(String nombre, String apellido, int edad, String fechaModificacion) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad= edad;
+		this.fechaModificacion = fechaModificacion;
 	}
 	
 	public Estudiante() {
@@ -60,6 +66,14 @@ public class Estudiante {
 
 	public void setEdad(int edad) {
 		this.edad = edad;
+	}
+
+	public String getFechaModificacion() {
+		return fechaModificacion;
+	}
+
+	public void setFechaModificacion(String fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
 	}	
 	
 	
