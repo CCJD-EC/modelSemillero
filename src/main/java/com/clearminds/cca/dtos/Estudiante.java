ac3ad7fbe6acb56adf8b6f40b1f2c634d328698d
Christian <jchristiandias@gmail.com>
2020-12-10 02:19:13 -0500

    Ultimo Trabajo

--- src/main/java/com/clearminds/cca/dtos/Estudiante.java
+++ src/main/java/com/clearminds/cca/dtos/Estudiante.java
@@ -18,6 +18,38 @@ public class Estudiante {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+	}
+	
+	public Estudiante() {
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}	
 	
 	
